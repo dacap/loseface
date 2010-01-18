4b71c27497f2698301facbaa2cf212feade67484
David Capello <davidcapello@gmail.com>
2010-01-18 23:16:09 +0000

    Updated copyright year.

--- src/Ann.h
+++ src/Ann.h
@@ -1,33 +1,6 @@
-// Lose Face - An open source face recognition project
-// Copyright (C) 2008-2009 David Capello
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-// * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in
-//   the documentation and/or other materials provided with the
-//   distribution.
-// * Neither the name of the authors nor the names of its contributors
-//   may be used to endorse or promote products derived from this
-//   software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
+// Copyright (C) 2008-2010 David Capello. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE.txt file.
 
 #ifndef LOSEFACE_ANN_H
 #define LOSEFACE_ANN_H
@@ -64,7 +37,7 @@ class Backpropagation;
 //////////////////////////////////////////////////////////////////////
 // Patterns
 //////////////////////////////////////////////////////////////////////
-
+
 /// A pair of input/output vectors.
 ///
 template<typename T>
@@ -155,7 +128,7 @@ public:
 //////////////////////////////////////////////////////////////////////
 // Activation functions
 //////////////////////////////////////////////////////////////////////
-
+
 /// Linear activation function.
 /// 
 template<typename T>
@@ -195,7 +168,7 @@ struct Radbas
 //////////////////////////////////////////////////////////////////////
 // Artificial Neural Networks
 //////////////////////////////////////////////////////////////////////
-
+
 /// A specific feedforward multilayer perceptron (MLP) neural network
 /// with 3 layers of neurons: input, hidden, output.
 /// 
@@ -540,7 +513,7 @@ public:
 //////////////////////////////////////////////////////////////////////
 // Helpers/Modifications for the Training Algorithms
 //////////////////////////////////////////////////////////////////////
-
+
 template<class Net>
 class WithMomentum
 {
